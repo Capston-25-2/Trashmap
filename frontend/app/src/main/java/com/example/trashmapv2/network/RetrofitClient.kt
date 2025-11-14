9e7b6ee58927c11df5b0726d3d0a016b03a226b4
민준영 <129071537+minjunyeong1@users.noreply.github.com>
2025-11-14 23:57:11 +0900

    보안 업데이트

--- frontend/app/src/main/java/com/example/trashmapv2/network/RetrofitClient.kt
+++ frontend/app/src/main/java/com/example/trashmapv2/network/RetrofitClient.kt
@@ -2,11 +2,10 @@ package com.example.trashmapv2.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.trashmapv2.BuildConfig
 
 object RetrofitClient {
-    // ⚠️ 매우 중요: 안드로이드 에뮬레이터에서 localhost는 에뮬레이터 자신을 가리킵니다.
-    // // 컴퓨터의 localhost에 접속하려면 '10.0.2.2' 주소를 사용해야 합니다.
-    private const val BASE_URL = "http://192.168.219.141:8000"
+    private val BASE_URL = BuildConfig.SERVER_BASE_URL
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
