f93c99cf9c2897ae1d3d6d54d395a5dbbaed63d2
ARanker <anhj445@naver.com>
2025-11-22 11:55:09 +0900

    PATCH /user/me API 구현

--- backend/api/user.py
+++ backend/api/user.py
@@ -32,3 +32,26 @@ async def delete_user_me(
     await db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# PATCH /user/me API
+@router.patch("/me", response_model=schemas.UserUpdate)
+async def patch_user_me(
+    update_data: schemas.UserUpdate,
+    current_user: models.User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db)
+):
+    current_user.username = update_data.username
+
+    try:
+        await db.commit()
+        await db.refresh(current_user)
+    except Exception as e:
+        # username은 unique해야하기 때문에
+        # 중복된 닉네임이면 DB 에러 발생
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="이미 사용 중인 닉네임입니다."
+        )
+
+    return current_user
