0d07b159ba56dad056775b3da5554c0f833aca19
ARanker <anhj445@naver.com>
2025-11-27 22:16:14 +0900

    fix: 기존 opinion 테이블 삭제에 따른 관계 삭제 feat: Suggest 테이블 생성

--- backend/model/models.py
+++ backend/model/models.py
@@ -39,8 +39,7 @@ class User(Base):
     trashcans = relationship("Trashcan", back_populates="user")
     reports = relationship("Report", back_populates="user")
     exp_history = relationship("ExpHistory", back_populates="user")
-    opinions = relationship("Opinion", back_populates="user")
-    
+    suggest = relationship("Suggest", back_populates="user")
 
     def __repr__(self):
         return f"<User(user_id={self.user_id}, username='{self.username}')>"
@@ -64,7 +63,6 @@ class Trashcan(Base):
     categories = relationship("TrashcanCategory", secondary=trashcan_to_category, back_populates="trashcans")
     reports = relationship("Report", back_populates="trashcan")
     issues = relationship("Issue", back_populates="trashcan")
-    opinions = relationship("Opinion", back_populates="trashcans")
 
     @hybrid_property
     def latitude(self):
@@ -148,3 +146,17 @@ class ExpHistory(Base):
     
     def __repr__(self):
         return f"<ExpHistory(exp_id={self.exp_id}, reason='{self.reason}')>"
+
+class Suggest(Base):
+    __tablename__ = 'suggest'
+    suggest_id = Column(BIGINT, primary_key=True)
+    user_id = Column(BIGINT, ForeignKey("users.user_id"), nullable=False)
+    geom = Column(Geometry(geometry_type='POINT', srid=4326), nullable=False)
+    dong = Column(VARCHAR, nullable=True) # 동 구분 로직 구현 완료 시 False로 변경 필요
+    status = Column(VARCHAR, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship("User", back_populates="suggest")
+
+    def __repr__(self):
+        return f"<Suggest(suggest_id={self.suggest_id}, dong='{self.dong}, status='{self.status})>"
