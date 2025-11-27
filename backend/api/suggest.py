31f2fa5d6d3c5b7d0969a817925fd6cb1165fac7
ARanker <anhj445@naver.com>
2025-11-27 22:51:25 +0900

    feat: POST suggest API 구현 (사용자용)

---
+++ backend/api/suggest.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Query, Response
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload # N + 1 문제 방지
+from sqlalchemy import func
+from geoalchemy2.shape import from_shape
+from shapely.geometry import Point
+
+from db.database import get_db
+from model import models
+from schema import schemas
+from api.auth import get_current_user
+
+router = APIRouter(
+    prefix="/suggest",
+    tags=["Suggest"]
+)
+
+# POST /suggest API 구현
+@router.post("/", response_model=schemas.SuggestCreationResponse, status_code=status.HTTP_201_CREATED)
+async def create_suggest(
+    suggest_data: schemas.SuggestCreate,
+    current_user: models.User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db)
+):
+    # 위도, 경도를 PostGIS의 POINT 형태로 변환
+    point_geom = from_shape(Point(suggest_data.lon, suggest_data.lat), srid = 4326)
+
+    # DB 데이터 조립
+    db_suggest = models.Suggest(
+        user_id = current_user.user_id,
+        geom = point_geom,
+        dong = suggest_data.dong,
+        status = 'pending'
+    )
+
+    # DB 등록
+    try:
+        db.add(db_suggest)
+        await db.commit()
+        await db.refresh(db_suggest)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="등록 오류가 발생했습니다."
+        )
+    
+    return schemas.SuggestCreationResponse(
+        suggest_id = db_suggest.suggest_id,
+        message="건의가 성공적으로 접수되었습니다."
+    )
